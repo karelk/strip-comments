0efdb5eb72235bf3f67d8be8baa1aa043e81a21b
karel <karel@localhost>
2023-03-16 12:35:30 +0100

    fix

--- stripcomments.c
+++ stripcomments.c
@@ -18,45 +18,35 @@ ssize_t parse_line(char *ln, ssize_t sz) {
 
     for (i = 0; i < sz; i++) {
 	    switch (ln[i]) {
-
 		case '#':
-
-		    if (i == 0) {
-			if (ln[1] != '!')
-			    return 0;
-		    }
+		    if (i == 0)
+			return 0;
 		    else {
+			// the magic happens here:
 			if (!single_q && !double_q)
 			    if ((curly <= 0) && (round <= 0))
 				if ((ln[i-1] != '$') && (ln[i-1] != '\\'))
 				    return i;
 		    }
 		    break;
-		    
 		case '\'':
 		    single_q = !single_q;
 		    break;
-
 		case '\"':
 		    double_q = !double_q;
 		    break;
-
 		case '{':
 		    curly++;
 		    break;
-
 		case '}':
 		    curly--;
 		    break;
-
 		case '(':
 		    round++;
 		    break;
-
 		case ')':
 		    round--;
 		    break;
-
 		default:
 		    break;
 	    }
@@ -88,24 +78,51 @@ int main(int argc, char **argv) {
         }
 
     ////////////////////////////////////////////////////////////////////////////
-    ///   read line   //////////////////////////////////////////////////////////
+    //   first line is a special case: check for shebang '#!'   ////////////////
     ////////////////////////////////////////////////////////////////////////////
 
-    while ((size = getline(&line, &len, f)) != -1) {
+    if ((size = getline(&line, &len, f)) != -1) {
 
-	cut = parse_line(line, size);
 
-	// if no cut was made, print line exacly
-	if (cut == size)
-	    printf("%s", line);
-	else {
-	    // strip any trailing spaces
-	    while ((cut > 0) && isspace(line[cut-1])) {
-		cut--;
+	if (line[0] == '#') {
+	    if ((size > 3) && (line[1] == '!'))
+		printf("%s", line);
 	}
-	    // do not print empty lines
-	   if (cut > 0)
-		printf("%.*s\n", cut, line);
+	else {
+	    cut = parse_line(line, size);
+
+	    // if no cut was made, print line exacly
+	    if (cut == size)
+		printf("%s", line);
+	    else {
+		// strip any trailing spaces
+		while ((cut > 0) && isspace(line[cut-1]))
+		    cut--;
+		// do not print empty lines
+		if (cut > 0)
+		    printf("%.*s\n", cut, line);
+	    }
+	}
+
+	////////////////////////////////////////////////////////////////////////
+	//   process the remaining lines in a loop   ///////////////////////////
+	////////////////////////////////////////////////////////////////////////
+
+	while ((size = getline(&line, &len, f)) != -1) {
+
+	    cut = parse_line(line, size);
+
+	    // if no cut was made, print line exacly
+	    if (cut == size)
+		printf("%s", line);
+	    else {
+		// strip any trailing spaces
+		while ((cut > 0) && isspace(line[cut-1]))
+		    cut--;
+		// do not print empty lines
+		if (cut > 0)
+		    printf("%.*s\n", cut, line);
+	    }
 	}
     }
 
