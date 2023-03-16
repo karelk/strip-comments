741b6251e45292f36d16e7b657318e3d6ffa1e8f
karel <karel@localhost>
2023-03-16 09:23:14 +0100

    refactoring

--- stripcomments.c
+++ stripcomments.c
@@ -4,71 +4,96 @@
 #include <stdbool.h>
 #include <unistd.h>
 
+////////////////////////////////////////////////////////////////////////////////
+///   function   ///////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+ssize_t parse_line(char *ln, ssize_t sz) {
+
+    ssize_t i;
+    int curly = 0;
+    int round = 0;
+    bool single_q = false;
+    bool double_q = false;
+
+    for (i = 0; i < sz; i++) {
+	    switch (ln[i]) {
+
+		case '#':
+
+		    if (i == 0) {
+			if (ln[1] != '!')
+			    return 0;
+		    }
+		    else {
+			if (!single_q && !double_q)
+			    if ((curly <= 0) && (round <= 0))
+				if ((ln[i-1] != '$') && (ln[i-1] != '\\'))
+				    return i;
+		    }
+		    break;
+		    
+		case '\'':
+		    single_q = !single_q;
+		    break;
+
+		case '\"':
+		    double_q = !double_q;
+		    break;
+
+		case '{':
+		    curly++;
+		    break;
+
+		case '}':
+		    curly--;
+		    break;
+
+		case '(':
+		    round++;
+		    break;
+
+		case ')':
+		    round--;
+		    break;
+
+		default:
+		    break;
+	    }
+    }
+    return sz;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+///   main   ///////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char **argv) {
 
     FILE *f;
-    ssize_t size;
     char *line = NULL;
     size_t len = 0;
-    int i, cut;
-    bool inside_single, inside_double, inside_curly, inside_round;
+    ssize_t size, cut;
 
-    if (!isatty(STDIN_FILENO) || argc < 2 || (argv[1] == "-")) {
-        // using standard input
+    ////////////////////////////////////////////////////////////////////////////
+    ///   open file   //////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+
+    if (!isatty(STDIN_FILENO) || argc < 2 || (argv[1] == "-"))
         f = stdin;
-    } else {
-        // try and open the first command line argument
+    else
         if (!(f = fopen(argv[1], "r"))) {
             perror(argv[1]);
             return 1;
         }
-    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    ///   read line   //////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
 
     while ((size = getline(&line, &len, f)) != -1) {
 
-	cut = size;
-
-	inside_curly = false;
-	inside_round = false;
-	inside_single = false;
-	inside_double = false;
-
-	// process line backwards
-	for (i = size-1; i >= 0; i--) {
-	    switch (line[i]) {
-		case '}':
-		    inside_curly = true;
-		    break;
-		case '{':
-		    inside_curly = false;
-		    break;
-		case ')':
-		    inside_round = true;
-		    break;
-		case '(':
-		    inside_round = false;
-		    break;
-		case '\'':
-		    inside_single = !inside_single;
-		    break;
-		case '\"':
-		    inside_double = !inside_double;
-		    break;
-		case '#':
-		    if (!inside_single && !inside_double && !inside_curly && !inside_round)
-			if (i>0) {
-			    // do not treat \# and $# as comments
-			    if ((line[i-1] != '\\') && (line[i-1] != '$'))
-				cut = i;
-			}
-			else
-			    cut=0;
-		    break;
-		default:
-		    break;
-	    }
-	}
+	cut = parse_line(line, size);
 
 	// if no cut was made, print line exacly
 	if (cut == size)
@@ -78,11 +103,15 @@ int main(int argc, char **argv) {
 	    while ((cut > 0) && isspace(line[cut-1])) {
 		cut--;
 	}
-	    // do not print modified line, if all that remains are whitespace characters
+	    // do not print empty lines
 	   if (cut > 0)
 		printf("%.*s\n", cut, line);
 	}
     }
+
+    ////////////////////////////////////////////////////////////////////////////
+    ///   finish   /////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
 
     fclose(f);
     if (line)
